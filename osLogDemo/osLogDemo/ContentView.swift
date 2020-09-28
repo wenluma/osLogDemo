750b79ec21f08810b2861b4b916d2210b34ac6ae
wenluma <wenluma_521@163.com>
2020-09-28 20:36:31 +0800

    init project

---
+++ osLogDemo/osLogDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  osLogDemo
+//
+//  Created by miao gaoliang on 2020/9/28.
+//  Copyright © 2020 miao gaoliang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
